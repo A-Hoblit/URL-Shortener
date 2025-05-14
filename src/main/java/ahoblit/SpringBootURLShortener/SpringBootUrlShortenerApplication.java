0d4ef73e8b59afbd67773edd47b01c7afb464617
Andrew Hoblit <70081309+A-Hoblit@users.noreply.github.com>
2025-05-14 15:51:22 -0700

    Basic Layout
    Set up a basic springleaf/Thymeleaf layout

--- src/main/java/ahoblit/SpringBootURLShortener/SpringBootUrlShortenerApplication.java
+++ src/main/java/ahoblit/SpringBootURLShortener/SpringBootUrlShortenerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootUrlShortenerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootUrlShortenerApplication.class, args);
 	}
 
